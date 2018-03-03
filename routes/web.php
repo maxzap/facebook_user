ef5268888c295bbe4dec04005058a9c8e21a1301
Maximiliano Zapata <maxzap@users.noreply.github.com>
2018-03-03 12:31:26 -0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('login', 'ProfileController@login')->name('login_fb');
+Route::get('portal', 'ProfileController@index')->name('portal_perfil');
+Route::get('perfil', 'ProfileController@profile')->name('detalle_perfil');
